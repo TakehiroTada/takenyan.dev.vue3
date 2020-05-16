efd78fa71b1c4c96734e70b9e72f1c507e393cf1
Takehiro Tada <taketada.works@gmail.com>
2020-05-17 03:28:45 +0900

    :wrench: hello-vue-next

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,21 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
-Vue.use(VueRouter);
-
-const routes: Array<RouteConfig> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+export const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: About
+    }
+  ]
 });
 
 export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div></div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
 
-export default {
+export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld
+  setup() {
+    const router = useRouter();
   }
-};
+});
 </script>
